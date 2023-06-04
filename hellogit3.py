7003a4b9f01145df7cde88d3725dde115ab9199f
JesCad <jesuscadena27@hotmail.com>
2023-06-04 15:59:40 -0600

    Aplicacion de git stash

--- hellogit3.py
+++ hellogit3.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-print("Se agrega fichero despues de tag y se a modificado esta linia v login")
-=======
+
 print("Se agrega fichero despues de tag y se a modificado esta linia v main")
->>>>>>> main
+print("ejemplo para guardar temporalmente con  git stash")
